85d2686ff9d8acc49b7865535ce7b79e4574508f
Nick Young <nick.young@gs.com>
2023-08-13 19:38:29 -0400

    Fix MANIFEST.in

--- pydantic_bind/share/cmake/pydantic-bind/pydantic-bindConfig.cmake
+++ pydantic_bind/share/cmake/pydantic-bind/pydantic-bindConfig.cmake
@@ -6,18 +6,18 @@ function(pydantic_bind_add_module target_name)
 
     set(module ${ARGN})
     string(REPLACE ".py" ".cpp" target_cpp "${generated_root}/${ARGN}")
-    string(REPLACE "../../.." "../../../pydantic_bind" target_cpp ${target_cpp})
+    string(REPLACE "../../../.." "../../.." target_cpp ${target_cpp})
     string(REPLACE ".cpp" ".h" target_header ${target_cpp})
 
     add_custom_command(
         OUTPUT "${PROJECT_SOURCE_DIR}/${target_cpp}" "${PROJECT_SOURCE_DIR}/${target_header}"
         DEPENDS ${ARGN}
-        COMMAND ${CMAKE_COMMAND} -E env PYTHONPATH=${PROJECT_SOURCE_DIR} python "../../../pydantic_bind/cpp_generator.py" -n ${target_name} --m ${module} -o ${output_dir}
+        COMMAND ${CMAKE_COMMAND} -E env PYTHONPATH=${PROJECT_SOURCE_DIR} python "../../../cpp_generator.py" -n ${target_name} --m ${module} -o ${output_dir}
         VERBATIM
     )
 
     pybind11_add_module(${target_name} "${PROJECT_SOURCE_DIR}/${target_cpp}")
     set_target_properties(${target_name} PROPERTIES PUBLIC_HEADER "${PROJECT_SOURCE_DIR}/${target_header}")
 
-    install(TARGETS ${target_name} COMPONENT python LIBRARY DESTINATION "../../..")
+    install(TARGETS ${target_name} COMPONENT python LIBRARY DESTINATION "../../../..")
 endfunction()
