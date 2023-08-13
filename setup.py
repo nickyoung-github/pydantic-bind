319fd8eaaf49aa1f58bff44ce0c083f952e0618a
Nick Young <nick.young@gs.com>
2023-08-13 19:13:45 -0400

    Fix packages

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(name='pydantic_bind',
-      version='1.0.2',
-      description='C++/pybind generation from Pydantic classes',
-      author='Nick Young',
+setup(name="pydantic_bind",
+      version="1.0.3",
+      description="C++/pybind generation from Pydantic classes",
+      author="Nick Young",
       license=r"https://www.apache.org/licenses/LICENSE-2.0",
-      packages=find_packages(),
+      packages=["pydantic_bind", "share"],
       install_requires=[
           "pydantic>=2.0.2",
           "pybind11"
+      ],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.11",
+          "Operating System :: OS Independent",
+          "License :: OSI Approved :: Apache Software License"
       ])
