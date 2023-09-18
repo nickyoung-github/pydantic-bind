0b1950606f629e10818497fa6366b83f5a33814f
nickyoung-github <nick.young.github@gmail.com>
2023-09-18 18:10:35 -0400

    Fix MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name="pydantic_bind",
-      version="1.0.9",
+      version="1.1.0",
       description="C++/pybind generation from Pydantic classes",
       author="Nick Young",
       license=r"https://www.apache.org/licenses/LICENSE-2.0",
