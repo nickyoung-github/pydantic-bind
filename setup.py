66629cc9d8ee461708aebb457749b148a3b5d02f
nickyoung-github <nick.young.github@gmail.com>
2023-09-18 18:12:33 -0400

    Work dammit !

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 # ToDo: Add versioneer
 
 setup(name="pydantic_bind",
-      version="1.1.7",
+      version="1.1.8",
       description="C++/pybind generation from Pydantic classes",
       author="Nick Young",
       license=r"https://www.apache.org/licenses/LICENSE-2.0",
